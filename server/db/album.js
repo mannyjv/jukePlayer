af3a24775f1c7d96c1e48835a47717b878087300
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2020-10-09 12:37:15 -0400

    One and two

---
+++ server/db/album.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+
+const db = require("./db");
+
+const Album = db.define('album', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  artworkUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "default-album.jpg"
+  }
+})
+
+
+
+module.exports = Album
+
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,27 @@
 const db = require('./db')
+const Album = require('./album')
+const Artist = require('./artist')
+const Song = require('./song')
+
 // require each of your models here...
 
 // ...and give them some nice associations here!
 
+Album.belongsTo(Artist)
+Artist.hasMany(Album)
+
+Song.belongsTo(Artist)
+Artist.hasMany(Song)
+
+Song.belongsTo(Album)
+Album.hasMany(Song)
+
+
 module.exports = {
   db,
+  Album,
+  Artist,
+  Song
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
 }
