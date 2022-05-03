725182ea52b4c0321e2555d2b4c08bd56eba6fbf
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2022-05-03 16:13:10 +0200

    feature play button
    add css to make play button disappear when user clicks on song, and song row will highlight dark red

--- client/Main.js
+++ client/Main.js
@@ -1,73 +1,78 @@
-import React from 'react'
-import axios from 'axios'
-import SideBar from './SideBar'
-import SingleAlbum from "./SingleAlbum"
-import AllAlbums from './AllAlbums'
-import Player from './Player'
+import React from 'react';
+import axios from 'axios';
+import SideBar from './SideBar';
+import SingleAlbum from './SingleAlbum';
+import AllAlbums from './AllAlbums';
+import Player from './Player';
 
 const audio = document.createElement('audio');
 
-
 export default class Main extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       albums: [],
       selectedAlbum: {},
-      currentSong: {}
-    }
-    this.selectAlbum = this.selectAlbum.bind(this)
-    this.deselectAlbum = this.deselectAlbum.bind(this)
+      currentSong: {},
+    };
+    this.selectAlbum = this.selectAlbum.bind(this);
+    this.deselectAlbum = this.deselectAlbum.bind(this);
     this.start = this.start.bind(this);
   }
 
   async componentDidMount() {
     try {
       const res = await axios.get('/api/albums');
-      const albums = res.data
-      this.setState({albums});
+      const albums = res.data;
+      this.setState({ albums });
     } catch (err) {
       console.log('There was a problem getting the albums!');
     }
   }
 
-  async selectAlbum (albumId) {
-      const res = await axios.get(`/api/albums/${albumId}`)
-      const selectedAlbum = res.data
-      this.setState({selectedAlbum})
+  async selectAlbum(albumId) {
+    const res = await axios.get(`/api/albums/${albumId}`);
+    const selectedAlbum = res.data;
+    this.setState({ selectedAlbum });
   }
 
   deselectAlbum() {
     this.setState({
-      selectedAlbum: {}
-    })
+      selectedAlbum: {},
+    });
   }
 
-  start (song, audioUrl) {
-    audio.src = audioUrl
+  start(song, audioUrl) {
+    audio.src = audioUrl;
     audio.load();
     audio.play();
 
     this.setState({
-      currentSong: song
-    })
+      currentSong: song,
+    });
   }
 
-  render () {
-
+  render() {
     return (
-      <div id='main' className='row container'>
+      <div id="main" className="row container">
         {/* The music starts here! */}
-        <SideBar deselectAlbum={this.deselectAlbum}/>
-        <div className='container'>
-        {
-          this.state.selectedAlbum.id ?
-            <SingleAlbum album={this.state.selectedAlbum} start={this.start} currentSong={this.state.currentSong}/> :
-            <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum}/>
-        }
+        <SideBar deselectAlbum={this.deselectAlbum} />
+        <div className="container">
+          {this.state.selectedAlbum.id ? (
+            <SingleAlbum
+              album={this.state.selectedAlbum}
+              start={this.start}
+              currentSong={this.state.currentSong}
+            />
+          ) : (
+            <AllAlbums
+              albums={this.state.albums}
+              selectAlbum={this.selectAlbum}
+            />
+          )}
         </div>
-        <Player/>
+        <Player />
       </div>
-    )
+    );
   }
 }
--- client/Songs.js
+++ client/Songs.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
-
 const Songs = (props) => {
-  const album = props.album
-  const {songs, artist} = album
-  const start = props.start
-  const currentSong = props.currentSong
+  const album = props.album;
+  const { songs, artist } = album;
+  const start = props.start;
+  const currentSong = props.currentSong;
 
-return (
-      songs.map((song, idx) => {
-          return <tr key={idx} className={song.id === currentSong.id ? 'active' : ''}>
-                  <td onClick={() => start(song, song.audioUrl)}><i className='fa fa-play-circle'/></td>
-                  <td>{idx + 1}</td>
-                  <td>{song.name}</td>
-                  <td>{artist.name}</td>
-                  <td>{song.genre}</td>
-                </tr>
-    })
-  )
-}
+  return songs.map((song, idx) => {
+    return (
+      <tr key={idx} className={song.id === currentSong.id ? 'active' : ''}>
+        <td onClick={() => start(song, song.audioUrl)}>
+          <i
+            className={song.id === currentSong.id ? '' : 'fa fa-play-circle'}
+          />
+        </td>
+        <td>{idx + 1}</td>
+        <td>{song.name}</td>
+        <td>{artist.name}</td>
+        <td>{song.genre}</td>
+      </tr>
+    );
+  });
+};
 export default Songs;
-
